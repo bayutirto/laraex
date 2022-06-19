a28344e19da640002786c7464570c1c324768b40
Tony Jemba <jjembatony@gmail.com>
2022-06-19 23:20:42 +0300

    using the script setup

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,14 +1,7 @@
-<script>
+<script setup>
     import { defineComponent } from 'vue';
     import AppLayout from '@/Layouts/AppLayout.vue';
     import Welcome from '@/Jetstream/Welcome.vue';
-
-    export default defineComponent({
-        components: {
-            AppLayout,
-            Welcome,
-        },
-    })
 </script>
 
 <template>
